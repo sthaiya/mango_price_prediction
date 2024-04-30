7ee7c02fa6a72f5e51c207af5c7d99070202a379
sthaiya <sthaiya@hotmail.com>
2024-04-30 16:21:47 -0400

    Changes

--- FinalProject.R
+++ FinalProject.R
@@ -68,15 +68,14 @@ print(skim(mango)) # view a pre-data-cleaning summary
 gg_miss_var(mango, show_pct = TRUE) # Identify and address missing data
 
 # Step 1: Some cleaning
-mango <- mango %>% 
-  remove_empty(which = c("rows", "cols")) %>% # Remove completely empty rows or columns
-  tidyr::drop_na() # Missing data is < 0.1% so can drop the missing entries
+mango <- mango %>%
+  remove_empty(which = c("rows", "cols"))# Remove completely empty rows or columns
 
 # filter(variety != "Other") %>% # remove where variety is other
 
 # Step 2: Identify and drop duplicates
 get_dupes(mango) %>% head(5) # show duplicates
-mango <- mango %>% distinct(.keep_all = TRUE) # remove duplicates
+# mango <- mango %>% distinct(.keep_all = TRUE) # remove duplicates
 
 # Step 3: show columns with only one value
 mango %>% purrr::keep(~length(unique(.x)) == 1) %>% head(5) # Show first 5 rows
@@ -105,6 +104,7 @@ ggplot(mango, aes(x=variety)) + geom_histogram(stat="count", colour="darkblue", 
   theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
 
 # one hot encoding 
+set.seed(2)
 cols_to_hold = c("arrival_date", "update_date", "min_price", "max_price", "modal_price")
 non_encoded_cols <- mango %>% select(any_of(cols_to_hold))
 to_encode <- mango %>% select(-any_of(cols_to_hold))
@@ -113,11 +113,7 @@ encoded_cols <- data.frame(predict(dmy, newdata = to_encode))
 mango <- cbind(non_encoded_cols, encoded_cols)
 
 # Some mutations before computations: Encode character data into numeric and extract month
-mango <- mango %>% 
-  mutate(
-    month = month(as.Date(arrival_date, format = "%d/%m/%Y")),
-    day = day(as.Date(arrival_date, format = "%d/%m/%Y"))
-  )
+mango$arrival_date <- as.Date(mango$arrival_date)
 
 # Recheck validation
 mango_validator()
@@ -130,7 +126,7 @@ mango_validator()
 #          p.mat = testRes$p, insig = 'blank') # shows min_price and max_price with correlation to our output variable we thus drop them
 
 # view a after cleaning summary
-cols_to_drop = c("arrival_date", "update_date", "min_price", "max_price")
+cols_to_drop = c("update_date", "min_price", "max_price")
 mango <- mango %>% select(-any_of(cols_to_drop))
 
 # How to deal with outliers
@@ -155,7 +151,6 @@ train_ind = createDataPartition(mango$modal_price, p = .7, list = F)
 train = mango[train_ind, ]
 test = mango[-train_ind, ]
 
-set.seed(1)
 tr_ctrl <- trainControl(
   method = "cv",  # Cross-validation
   number = 5,     # 5-fold cross-validation
@@ -182,113 +177,10 @@ d_test_data = xgb.DMatrix(data=ts_input_x, label = ts_input_y)
 
 watchlist = list(train=d_train_data, test=d_test_data)
 
-# model = xgb.train(data = d_train_data, max.depth = 6, watchlist=watchlist, nrounds = 450)
-# From the output, the RMSE just keeps dropping as the number of rounds is increased, so let's use MAE as the stopping criterion rather than RMSE.
-# MAE is less sensitive to outliers compared to RMSE because MAE does not square the errors. If your dataset contains significant outliers that you do not want to heavily penalize, MAE might be a better choice.
-# Will perform cross-validation to validate the results from the XGBoost model
-# Specifying evaluation metrics and displaying training and testing data at each of the 100 rounds (using MAE)
-# model <- xgb.train(data = xgb_train, max.depth = 3, watchlist = watchlist, nrounds = 100,
-#                    eval_metric = "mae")  # Using MAE as the evaluation metric
-#From the output we can see that the minimum testing MAE is achieved at the 72nd round. Beyond this point, the test MSE fluctuates instead of being always decreasing.
-#To avoid overfitting the data, we will fit the final XGBoost model with 72 rounds.
-
 xgb_model = xgboost(data=d_train_data, max.depth=6, nrounds = 100, verbose = FALSE)
 xgb_pred <- predict(xgb_model, newdata = d_test_data)
 
-
-
-
-grid <- expand.grid(mtry = c(2, 3, 4),        # Number of variables to sample as candidates at each split
-                    ntree = c(500, 1000, 1500))
-
-rf_tuned <- train(Species ~ .,                   # Formula for the model
-                  data = data,                   # Training data
-                  method = "rf",                 # Random Forest method
-                  trControl = ctrl,              # Training control settings
-                  tuneGrid = grid)
-
-
-grid <- expand.grid(mtry = c(2, 3, 4),        # Number of variables to sample as candidates at each split
-                    ntree = c(500, 1000, 1500))
-
-rf_tuned <- train(modal_price ~ .,                   # Formula for the model
-                  data = mango,                   # Training data
-                  method = "rf",                 # Random Forest method
-                  trControl = tr_ctrl,              # Training control settings
-                  tuneGrid = grid)
-
-
-# XGBoost
-input_x <- as.matrix(select(train, -modal_price))
-input_y <- train$modal_price
-
-grid_default <- expand.grid(
-  nrounds = 100,
-  max_depth = 6,
-  eta = 0.3,
-  gamma = 0,
-  colsample_bytree = 1,
-  min_child_weight = 1,
-  subsample = 1
-)
-
-train_control <- trainControl(
-  method = "cv",
-  number = 5, # with n folds 
-  verboseIter = FALSE, # no training log
-  allowParallel = FALSE # FALSE for reproducible results 
-)
-
-xgb_base <- caret::train(
-  x = input_x,
-  y = input_y,
-  trControl = train_control,
-  tuneGrid = grid_default,
-  method = "xgbTree",
-  verbose = FALSE
-)
-
-xgb_base
-
-# We start with nrounds from 200 as lower rounds gives very big errors
-tune_grid <- expand.grid(
-  nrounds = seq(from = 200, to = 1000, by = 50),
-  eta = c(0.025, 0.05, 0.1, 0.3),
-  max_depth = c(2, 3, 4, 5, 6),
-  gamma = 0,
-  colsample_bytree = 1,
-  min_child_weight = 1,
-  subsample = 1
-)
-
-tune_control <- caret::trainControl(
-  method = "cv", # cross-validation
-  number = 5, # with n folds 
-  verboseIter = FALSE, # no training log
-  allowParallel = FALSE # FALSE for reproducible results 
-)
-
-xgb_tune <- caret::train(
-  x = input_x,
-  y = input_y,
-  trControl = tune_control,
-  tuneGrid = tune_grid,
-  method = "xgbTree",
-  verbose = FALSE
-)
-
-tuneplot <- function(x, probs = .90) {
-  ggplot(x) +
-    coord_cartesian(ylim = c(quantile(x$results$RMSE, probs = probs), min(x$results$RMSE))) +
-    theme_bw()
-}
-
-tuneplot(xgb_tune)
-
-# We get the best tuning parameters
-xgb_tune$bestTune
-
-# Final results
+# Test results
 df_res = data.frame(
   model = c("Random Forest", "SVR", "XGBoost"),
   RMSE = c(
@@ -309,8 +201,23 @@ df_res = data.frame(
 )
 df_res
 
+# Tuning for RF Model
+tune_control <- trainControl(method="cv", number=5, search="grid")
+tune_grid <- expand.grid(mtry = c(1:10),        # Number of variables to sample as candidates at each split
+                    ntree = c(500, 1000, 1500))
 
+rf_tuned <- train(modal_price ~ .,                   # Formula for the model
+                  data = train,                   # Training data
+                  method = "rf",                 # Random Forest method
+                  trControl = tune_control,              # Training control settings
+                  tuneGrid = tune_grid)
 
+# control <- trainControl(method="repeatedcv", number=10, repeats=3, search="grid")
+# set.seed(seed)
+# tunegrid <- expand.grid(.mtry=c(1:15))
+# rf_gridsearch <- train(Class~., data=dataset, method="rf", metric=metric, tuneGrid=tunegrid, trControl=control)
+# print(rf_gridsearch)
+# plot(rf_gridsearch)
 
-# Save model to RDS file
-saveRDS(xgb_tune, paste0(script_path(), .Platform$file.sep, "model.rds"))
+# Save the tuned model to RDS file
+saveRDS(rf_tuned, paste0(script_path(), .Platform$file.sep, "model.rds"))
